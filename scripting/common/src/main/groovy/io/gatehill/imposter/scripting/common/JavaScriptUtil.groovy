d09e41eb64a8a448868c041cfb5d2b8562c599f8
Pete Cornish <outofcoffee@gmail.com>
2021-05-19 16:09:58 +0100

    Generates JavaScript DSL once.

--- scripting/common/src/main/groovy/io/gatehill/imposter/scripting/common/JavaScriptUtil.groovy
+++ scripting/common/src/main/groovy/io/gatehill/imposter/scripting/common/JavaScriptUtil.groovy
@@ -9,26 +9,32 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 /**
- * @author pete
+ * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
 class JavaScriptUtil {
     private static final Logger LOGGER = LogManager.getLogger(JavaScriptUtil.class);
 
+    private static final String DSL_FUNCTIONS = buildDslFunctions()
+
     private JavaScriptUtil() {}
 
+    /**
+     * Expose superclass methods as DSL functions.
+     *
+     * @return the JavaScript function variables
+     */
+    static String buildDslFunctions() {
+        def dslMethods = MutableResponseBehaviour.class.declaredMethods.collect { method -> method.name }
+
+        return dslMethods.unique()
+                .collect { methodName -> "var ${methodName} = Java.super(responseBehaviour).${methodName};" }
+                .join('\r\n')
+    }
+
     static String wrapScript(Path scriptFile) throws IOException {
-        // expose DSL methods
-        def dslMethods = []
-        MutableResponseBehaviour.class.declaredMethods.each { method -> dslMethods += method.name }
-
-        def dslFunctions = ""
-        dslMethods.unique().each {
-            methodName -> dslFunctions += "var ${methodName} = Java.super(responseBehaviour).${methodName};\r\n"
-        }
-
         // wrap mock script
         def mockScript = new String(Files.readAllBytes(scriptFile))
-        def wrappedScript = buildWrappedScript(dslFunctions, mockScript)
+        def wrappedScript = buildWrappedScript(DSL_FUNCTIONS, mockScript)
 
         LOGGER.trace("Wrapped script: ${wrappedScript}")
         return wrappedScript
