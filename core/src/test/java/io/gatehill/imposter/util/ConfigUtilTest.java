2c43df8baf522294a2ef31a9378deebfde7f8bb5
Pete Cornish <outofcoffee@gmail.com>
2021-10-27 12:43:15 +0100

    refactor: moves core util tests to java source directory.

--- core/src/test/java/io/gatehill/imposter/util/ConfigUtilTest.java
+++ core/src/test/java/io/gatehill/imposter/util/ConfigUtilTest.java
@@ -41,61 +41,67 @@
  * along with Imposter.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package io.gatehill.imposter.util
+package io.gatehill.imposter.util;
 
+import io.gatehill.imposter.plugin.PluginManager;
+import io.gatehill.imposter.plugin.config.PluginConfigImpl;
+import org.junit.Before;
+import org.junit.Test;
 
-import io.gatehill.imposter.plugin.PluginManager
-import io.gatehill.imposter.plugin.config.PluginConfigImpl
-import org.junit.Before
-import org.junit.Test
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertEquals;
 
 /**
  * Tests for {@link ConfigUtil}.
  *
  * @author Pete Cornish {@literal <outofcoffee@gmail.com>}
  */
-class ConfigUtilTest {
-    PluginManager pluginManager
+public class ConfigUtilTest {
+    PluginManager pluginManager;
 
     @Before
-    void setUp() throws Exception {
-        pluginManager = new PluginManager()
+    public void setUp() throws Exception {
+        pluginManager = new PluginManager();
     }
 
     @Test
-    void testLoadPluginConfigs() {
-        def configDir = new File(ConfigUtilTest.class.getResource('/config').toURI()).path
+    public void testLoadPluginConfigs() throws Exception {
+        String configDir = new File(ConfigUtilTest.class.getResource("/config").toURI()).getPath();
 
-        def configs = ConfigUtil.loadPluginConfigs(pluginManager, [configDir] as String[])
-        assertEquals(1, configs.size())
+        Map<String, List<File>> configs = ConfigUtil.loadPluginConfigs(pluginManager, new String[]{configDir});
+        assertEquals(1, configs.size());
 
-        def configFiles = configs['io.gatehill.imposter.core.test.ExamplePluginImpl']
-        assertEquals(2, configFiles.size())
+        List<File> configFiles = configs.get("io.gatehill.imposter.core.test.ExamplePluginImpl");
+        assertEquals(2, configFiles.size());
     }
 
     @Test
-    void testLoadLegacyPluginConfig() {
-        def configDir = new File(ConfigUtilTest.class.getResource('/legacy').toURI()).path
+    public void testLoadLegacyPluginConfig() throws Exception {
+        String configDir = new File(ConfigUtilTest.class.getResource("/legacy").toURI()).getPath();
 
-        def configs = ConfigUtil.loadPluginConfigs(pluginManager, [configDir] as String[])
-        assertEquals(1, configs.size())
+        Map<String, List<File>> configs = ConfigUtil.loadPluginConfigs(pluginManager, new String[]{configDir});
+        assertEquals(1, configs.size());
 
-        def configFiles = configs['io.gatehill.imposter.core.test.ExamplePluginImpl']
-        assertEquals(1, configFiles.size())
+        List<File> configFiles = configs.get("io.gatehill.imposter.core.test.ExamplePluginImpl");
+        assertEquals(1, configFiles.size());
     }
 
     @Test
-    void testLoadInterpolatedPluginConfig() {
+    public void testLoadInterpolatedPluginConfig() throws Exception {
         // override environment variables in string interpolators
-        def environment = ["EXAMPLE_PATH": "/test"]
-        ConfigUtil.initInterpolators(environment)
+        Map<String, String> environment = new HashMap<String, String>() {{
+            put("EXAMPLE_PATH", "/test");
+        }};
+        ConfigUtil.initInterpolators(environment);
 
-        def configFile = new File(ConfigUtilTest.class.getResource('/interpolated/test-config.yaml').toURI())
+        File configFile = new File(ConfigUtilTest.class.getResource("/interpolated/test-config.yaml").toURI());
 
-        def config = ConfigUtil.loadPluginConfig(configFile, PluginConfigImpl.class, true, true)
+        PluginConfigImpl config = ConfigUtil.loadPluginConfig(configFile, PluginConfigImpl.class, true, true);
 
-        assertEquals("/test", config.path)
+        assertEquals("/test", config.getPath());
     }
 }
