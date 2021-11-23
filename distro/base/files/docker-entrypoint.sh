ffd9fedacf2c88696c248a02e399895115492beb
Pete Cornish <outofcoffee@gmail.com>
2021-11-23 18:57:25 +0000

    feat: adds support for CLI boot.

--- distro/base/files/docker-entrypoint.sh
+++ distro/base/files/docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 set -e
 
-if [ -z "${JAVA_OPTS}" ]; then
-  export JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
-fi
+if [ "${IMPOSTER_BOOT}" = "cli" ]; then
+  # cli boot
+  export IMPOSTER_CLI_LOG_LEVEL="${IMPOSTER_CLI_LOG_LEVEL:-INFO}"
+  export IMPOSTER_LOG_LEVEL="${IMPOSTER_LOG_LEVEL:-DEBUG}"
+  exec imposter up /opt/imposter/config
 
-exec java ${JAVA_ARGS} ${JAVA_OPTS} -jar /opt/imposter/lib/imposter-${DISTRO_NAME}.jar "$@"
+else
+  # direct JVM boot
+  if [ -z "${JAVA_OPTS}" ]; then
+    export JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
+  fi
+  exec java ${JAVA_ARGS} ${JAVA_OPTS} -jar /opt/imposter/lib/imposter.jar "$@"
+fi
