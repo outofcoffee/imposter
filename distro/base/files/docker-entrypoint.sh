2501470457d57a04f6b8d55ac0fb8910fc1a18c5
Pete Cornish <outofcoffee@gmail.com>
2021-12-15 18:07:33 +0000

    feat: switches boot to CLI unpacked method.

--- distro/base/files/docker-entrypoint.sh
+++ distro/base/files/docker-entrypoint.sh
@@ -1,17 +1,39 @@
-#!/usr/bin/env sh
+#!/usr/bin/env ash
 set -e
 
-if [ "${IMPOSTER_BOOT}" = "cli" ]; then
-  # cli boot
+function boot_direct_jvm() {
+    exec "/opt/imposter/bin/imposter" "$@"
+}
+
+function boot_cli() {
   export IMPOSTER_CLI_LOG_LEVEL="${IMPOSTER_CLI_LOG_LEVEL:-INFO}"
   export IMPOSTER_LOG_LEVEL="${IMPOSTER_LOG_LEVEL:-DEBUG}"
-  exec imposter up /opt/imposter/config
 
+  CONFIG_DIR="/opt/imposter/config"
+  IMPOSTER_ARGS=
+  while [ $# -gt 0 ]; do
+    case $1 in
+    "--configDir")
+      shift 1
+      CONFIG_DIR="$1"
+      ;;
+    "--listenPort")
+      shift 1
+      IMPOSTER_ARGS="${IMPOSTER_ARGS} --port $1"
+      ;;
+    esac
+    shift 1
+  done
+  exec imposter up ${IMPOSTER_ARGS} "${CONFIG_DIR}"
+}
+
+if [[ -z "${JAVA_OPTS}" ]]; then
+  export JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
+fi
+export JAVA_OPTS="$JAVA_ARGS $JAVA_OPTS"
+
+if [[ "${IMPOSTER_BOOT}" == "classic" ]]; then
+  boot_direct_jvm "$@"
 else
-  # direct JVM boot
-  if [ -z "${JAVA_OPTS}" ]; then
-    export JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
-  fi
-  export JAVA_OPTS="$JAVA_ARGS $JAVA_OPTS"
-  exec "/opt/imposter/bin/imposter" "$@"
+  boot_cli "$@"
 fi
