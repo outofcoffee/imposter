8c454e53bd2bd606248db471fba2ebd8a3146ecf
Pete Cornish <outofcoffee@gmail.com>
2021-12-21 20:52:57 +0000

    feat: hardens container image to remove alpine distro bin.

--- distro/base/files/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ash
-set -e
-
-if [[ -z "${JAVA_OPTS}" ]]; then
-  export JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
-fi
-export JAVA_OPTS
-
-exec "/opt/imposter/bin/imposter" "$@"
