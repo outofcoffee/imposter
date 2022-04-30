fb8cf73a30e928dee4e1ec3e344113981e48568e
Pete Cornish <outofcoffee@gmail.com>
2022-04-30 22:39:18 +0100

    build!: drops support for Java 8.
    BREAKING CHANGE: Imposter no longer supports Java 8, due to the complexity of maintaining multiple codepaths depending on target JVMs.

--- scripting/nashorn-embedded/src/test/resources/script/require_types.js
+++
@@ -1,9 +0,0 @@
-var imposter = require("@imposter-js/types");
-var context = imposter.context;
-var respond = imposter.respond;
-
-var exampleHeader = context.request.headers['X-Example'];
-
-respond()
-    .withStatusCode(201)
-    .withData(exampleHeader);
