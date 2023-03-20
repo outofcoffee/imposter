98bfc75a656ad123b8246b0481248be6561cf36c
Pete Cornish <outofcoffee@gmail.com>
2023-03-20 12:45:17 +0000

    test: improves coverage of templated SOAP responses.

---
+++ mock/soap/src/test/java/io/gatehill/imposter/plugin/soap/TemplateResponseTest.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This file is part of Imposter.
+ *
+ * "Commons Clause" License Condition v1.0
+ *
+ * The Software is provided to you by the Licensor under the License, as
+ * defined below, subject to the following condition.
+ *
+ * Without limiting other conditions in the License, the grant of rights
+ * under the License will not include, and the License does not grant to
+ * you, the right to Sell the Software.
+ *
+ * For purposes of the foregoing, "Sell" means practicing any or all of
+ * the rights granted to you under the License to provide to third parties,
+ * for a fee or other consideration (including without limitation fees for
+ * hosting or consulting/support services related to the Software), a
+ * product or service whose value derives, entirely or substantially, from
+ * the functionality of the Software. Any license notice or attribution
+ * required by the License must also include this Commons Clause License
+ * Condition notice.
+ *
+ * Software: Imposter
+ *
+ * License: GNU Lesser General Public License version 3
+ *
+ * Licensor: Peter Cornish
+ *
+ * Imposter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Imposter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Imposter.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.gatehill.imposter.plugin.soap
+
+import io.gatehill.imposter.plugin.soap.util.SoapUtil
+import io.gatehill.imposter.server.BaseVerticleTest
+import io.gatehill.imposter.util.HttpUtil
+import io.restassured.RestAssured
+import io.vertx.ext.unit.TestContext
+import org.hamcrest.Matchers
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Templates response message from request body.
+ *
+ * @author Pete Cornish
+ */
+class TemplateResponseTest : BaseVerticleTest() {
+    override val pluginClass = SoapPluginImpl::class.java
+    override val testConfigDirs = listOf("/template-response")
+    private val soapEnvNamespace = SoapUtil.soap12RecEnvNamespace
+    private val soapContentType = SoapUtil.soap12ContentType
+
+    @Before
+    @Throws(Exception::class)
+    override fun setUp(testContext: TestContext) {
+        super.setUp(testContext)
+        RestAssured.baseURI = "http://$host:$listenPort"
+    }
+
+    @Test
+    fun `template response from request body`() {
+        val getPetByIdEnv = SoapUtil.wrapInEnv(
+            """
+<getPetByIdRequest xmlns="urn:com:example:petstore">
+  <id>123</id>
+</getPetByIdRequest>
+""".trim(), soapEnvNamespace
+        )
+
+        RestAssured.given()
+            .log().ifValidationFails()
+            .accept(soapContentType)
+            .contentType(soapContentType)
+            .`when`()
+            .body(getPetByIdEnv)
+            .post("/pets/")
+            .then()
+            .log().ifValidationFails()
+            .statusCode(HttpUtil.HTTP_OK)
+            .body(
+                Matchers.allOf(
+                    Matchers.containsString("Envelope"),
+                    Matchers.containsString("getPetByIdResponse"),
+                    Matchers.containsString("123"),
+                )
+            )
+    }
+}
