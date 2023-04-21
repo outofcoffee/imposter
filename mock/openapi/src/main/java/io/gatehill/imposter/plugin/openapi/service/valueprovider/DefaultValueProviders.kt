bee62cc2cc1e4c957fb1c69a7a4da54c8b6ee31b
Pete Cornish <outofcoffee@gmail.com>
2023-04-21 20:59:11 +0100

    refactor(openapi): inlines trivial default value providers.

--- mock/openapi/src/main/java/io/gatehill/imposter/plugin/openapi/service/valueprovider/DefaultValueProviders.kt
+++ mock/openapi/src/main/java/io/gatehill/imposter/plugin/openapi/service/valueprovider/DefaultValueProviders.kt
@@ -8,10 +8,16 @@ import java.time.temporal.ChronoUnit
 import java.util.*
 
 internal val DEFAULT_VALUE_PROVIDERS: Map<String, DefaultValueProvider<*>> = mapOf(
-    "string" to StringDefaultValueProvider(),
-    "number" to NumberDefaultValueProvider(),
-    "integer" to IntegerDefaultValueProvider(),
-    "boolean" to BooleanDefaultValueProvider()
+        "string" to StringDefaultValueProvider(),
+        "number" to object : DefaultValueProvider<Double> {
+            override fun provide(schema: Schema<*>) = 0.0
+        },
+        "integer" to object : DefaultValueProvider<Int> {
+            override fun provide(schema: Schema<*>) = 0
+        },
+        "boolean" to object : DefaultValueProvider<Boolean> {
+            override fun provide(schema: Schema<*>) = false
+        },
 )
 
 internal interface DefaultValueProvider<T> {
@@ -25,36 +31,20 @@ internal class StringDefaultValueProvider : DefaultValueProvider<String> {
             // see https://swagger.io/docs/specification/data-models/data-types/
             when (schema.format) {
                 "date" -> return DateTimeUtil.DATE_FORMATTER.format(
-                    LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)
+                        LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)
                 )
                 "date-time" -> return DateTimeUtil.DATE_TIME_FORMATTER.format(
-                    OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS)
+                        OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS)
                 )
                 "password" -> return "changeme"
-                "byte" ->                         // base64-encoded characters
-                    return "SW1wb3N0ZXI0bGlmZQo="
+
+                // base64-encoded characters
+                "byte" -> return "SW1wb3N0ZXI0bGlmZQo="
+
                 "email" -> return "test@example.com"
                 "uuid", "guid" -> return UUID.randomUUID().toString()
                 else -> ""
             }
         } ?: ""
-    }
-}
-
-internal class NumberDefaultValueProvider : DefaultValueProvider<Double> {
-    override fun provide(schema: Schema<*>): Double {
-        return 0.0
-    }
-}
-
-internal class IntegerDefaultValueProvider : DefaultValueProvider<Int> {
-    override fun provide(schema: Schema<*>): Int {
-        return 0
-    }
-}
-
-internal class BooleanDefaultValueProvider : DefaultValueProvider<Boolean> {
-    override fun provide(schema: Schema<*>): Boolean {
-        return false
     }
 }
