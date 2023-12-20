6644033d389fea97e25445cb0b80555f38819044
Pete Cornish <outofcoffee@gmail.com>
2023-12-20 21:49:52 +0000

    test: adds local docs test script.

---
+++ scripts/docs/Dockerfile
@@ -0,0 +1,3 @@
+FROM squidfunk/mkdocs-material
+
+RUN pip install mkdocs-mermaid2-plugin
---
+++ scripts/local-docs.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2023.
+#
+# This file is part of Imposter.
+#
+# "Commons Clause" License Condition v1.0
+#
+# The Software is provided to you by the Licensor under the License, as
+# defined below, subject to the following condition.
+#
+# Without limiting other conditions in the License, the grant of rights
+# under the License will not include, and the License does not grant to
+# you, the right to Sell the Software.
+#
+# For purposes of the foregoing, "Sell" means practicing any or all of
+# the rights granted to you under the License to provide to third parties,
+# for a fee or other consideration (including without limitation fees for
+# hosting or consulting/support services related to the Software), a
+# product or service whose value derives, entirely or substantially, from
+# the functionality of the Software. Any license notice or attribution
+# required by the License must also include this Commons Clause License
+# Condition notice.
+#
+# Software: Imposter
+#
+# License: GNU Lesser General Public License version 3
+#
+# Licensor: Peter Cornish
+#
+# Imposter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Imposter is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Imposter.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+set -e
+
+# This script is used to view the documentation site locally.
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( cd "${SCRIPT_DIR}"/../ && pwd )"
+
+cd "${SCRIPT_DIR}/docs"
+docker build -t imposter-docs .
+
+docker run --rm -it -p 8000:8000 \
+  -v "${ROOT_DIR}/mkdocs.yml:/docs/mkdocs.yml:ro" \
+  -v "${ROOT_DIR}/docs:/docs/docs:ro" \
+  imposter-docs
