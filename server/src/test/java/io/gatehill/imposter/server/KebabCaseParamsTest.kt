c8083aa46a31493879f311ff95e075e2da02738d
Pete Cornish <outofcoffee@gmail.com>
2024-04-24 09:29:37 +0100

    test: adds test for kebab-case path params.

---
+++ server/src/test/java/io/gatehill/imposter/server/KebabCaseParamsTest.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This file is part of Imposter.
+ *
+ * "Commons Clause" License Condition v1.0
+ *
+ * The Software is provided to you by the Licensor under the License, as
+ * defined below, subject to the following condition.
+ *
+ * Without limiting other conditions in the License, the grant of rights
+ * under the License will not include, and the License does not grant to
+ * you, the right to Sell the Software.
+ *
+ * For purposes of the foregoing, "Sell" means practicing any or all of
+ * the rights granted to you under the License to provide to third parties,
+ * for a fee or other consideration (including without limitation fees for
+ * hosting or consulting/support services related to the Software), a
+ * product or service whose value derives, entirely or substantially, from
+ * the functionality of the Software. Any license notice or attribution
+ * required by the License must also include this Commons Clause License
+ * Condition notice.
+ *
+ * Software: Imposter
+ *
+ * License: GNU Lesser General Public License version 3
+ *
+ * Licensor: Peter Cornish
+ *
+ * Imposter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Imposter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Imposter.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.gatehill.imposter.server
+
+import io.gatehill.imposter.plugin.test.TestPluginImpl
+import io.restassured.RestAssured
+import io.vertx.ext.unit.TestContext
+import io.vertx.ext.unit.junit.VertxUnitRunner
+import org.hamcrest.Matchers.equalTo
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Tests for kebab-case path parameters.
+ *
+ * @author Pete Cornish
+ */
+@RunWith(VertxUnitRunner::class)
+class KebabCaseParamsTest : BaseVerticleTest() {
+    override val pluginClass = TestPluginImpl::class.java
+
+    @Before
+    @Throws(Exception::class)
+    override fun setUp(testContext: TestContext) {
+        super.setUp(testContext)
+        RestAssured.baseURI = "http://$host:$listenPort"
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+    }
+
+    override val testConfigDirs = listOf(
+        "/kebab-case-params"
+    )
+
+    @Test
+    fun `should handle kebab-case params`() {
+        RestAssured.given().`when`()
+            .get("/foo/bar")
+            .then()
+            .statusCode(equalTo(201))
+            .body(equalTo("bar"))
+    }
+}
